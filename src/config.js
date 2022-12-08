584e47d7e4943d3f38c5d2b8136159e2f1f86897
yonatankumsa <guitaristyona@gmail.com>
2022-12-08 12:38:05 -0500

    yo

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://post-it-yo-ralc.onrender.com";
+let BASE_URL = process.env.NODE_ENV;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:4000/";
 }
